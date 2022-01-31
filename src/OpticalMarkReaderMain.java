bd6b0e2e9967fbb5e41d678417514c431060be82
roba2713 <rohan.d.bhandarkar@gmail.com>
2022-01-31 12:52:27 -0800

    readfiles abt ur mom

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class OpticalMarkReaderMain {
     public static void main(String[] args) {
         String pathToPdf = fileChooser("omrtest.pdf");
         System.out.println("Loading pdf at " + pathToPdf);
+        String fileContents = readFile("omrtest.pdf");
 
 
         /*
@@ -15,6 +18,37 @@ public class OpticalMarkReaderMain {
         (4).  Output 2 csv files
          */
 
+    }
+    public static String readFile(String filePath) {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath));) {
+
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.getProperty("line.separator"));
+                line = br.readLine();
+            }
+
+        } catch (Exception errorObj) {
+            System.err.println("There was a problem reading the " + filePath);
+        }
+
+        return sb.toString();
+    }
+
+    public static void writeToFile(String filePath, String data) {
+        try (FileWriter f = new FileWriter(filePath);
+             BufferedWriter b = new BufferedWriter(f);
+             PrintWriter writer = new PrintWriter(b);) {
+
+            writer.print(data);
+
+        } catch (Exception errorObj) {
+            System.out.println("There was an error with the file");
+            errorObj.printStackTrace();
+        }
     }
 
     private static String fileChooser(String s) {
