36ab4486e2424a32cb9bc1f97de8b9367430a46f
roba2713 <rohan.d.bhandarkar@gmail.com>
2022-02-03 11:54:38 -0800

    readfiles abt ur mom

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -6,13 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class OpticalMarkReaderMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String pathToPdf = fileChooser("omrtest.pdf");
         System.out.println("Loading pdf at " + pathToPdf);
-        String fileContents = readFile("omrtest.pdf");
 
 
-       readFile("C:\Users\adame\IdeaProjects\ImageProcessingOpticalMark\assets\omrtest.pdf");
+
         /*
         Your code here to...
         (1).  Load the pdf
@@ -24,22 +23,7 @@ public class OpticalMarkReaderMain {
          */
 
     }
-    public static String readFile(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
-    }
 
-    public static void writeToFile(String filePath, String data) {
-        try (FileWriter f = new FileWriter(filePath);
-             BufferedWriter b = new BufferedWriter(f);
-             PrintWriter writer = new PrintWriter(b);) {
-
-            writer.print(data);
-
-        } catch (Exception errorObj) {
-            System.out.println("There was an error with the file");
-            errorObj.printStackTrace();
-        }
-    }
 
     private static String fileChooser(String s) {
         String userDirLocation = System.getProperty("user.dir");
