cc40c7cc4dc87f68c22ccfcc28ec13972ae82918
BestBeast05 <aelbahey268@student.fuhsd.org>
2022-02-07 12:15:41 -0800

    generate arraylist of student answers and check them vs key for correct %

--- src/FilterTest.java
+++ src/FilterTest.java
@@ -5,6 +5,7 @@ import core.DImage;
 import core.DisplayWindow;
 import processing.core.PImage;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FilterTest {
@@ -23,57 +24,60 @@ public class FilterTest {
         ArrayList<DImage> images = new ArrayList<>();
 
 
-       ArrayList<PImage> inimage  = PDFHelper.getPImagesFromPdf(filepath);
+        ArrayList<PImage> inimage = PDFHelper.getPImagesFromPdf(filepath);
         for (PImage pImage : inimage) {
             images.add(new DImage(pImage));
 
 
         }
         return images;
-                                   // that does the image processing an returns a DTO with
-                                   // the information you want
+        // that does the image processing an returns a DTO with
+        // the information you want
     }
 
-    public int blackcpixelsinregion (short [][] inputarray , int thresholdbwtnquestions, int thresholdbwtwnsolutions, int inputrow, int inputcol, int numOptions,int distanceToFirstQuestionhorizontal) {
+    public static int blackcpixelsinregion(short[][] inputarray, int thresholdbwtnquestions, int thresholdbwtwnsolutions, int inputrow, int inputcol, int numOptions, int distanceToFirstQuestionhorizontal) {
         int blackpixels = 0;
         int blackcounter = 0;
-        int storedquadrant =0;
+        int storedquadrant = 0;
         int storedwor = 0;
-                for (int k = inputrow; k < inputrow + thresholdbwtnquestions; k++) {
+        for (int k = inputrow; k < inputrow + thresholdbwtnquestions; k++) {
 
 
-
-                    for (int x = inputcol; x < inputcol + thresholdbwtwnsolutions; x++) {
-                        if (inputarray[k][x] < 150) {
-                            blackcounter++;
-                            storedwor=x;
-                        }
-
+            for (int x = inputcol; x < inputcol + thresholdbwtwnsolutions; x++) {
+                if (inputarray[k][x] < 150) {
+                    blackcounter++;
+                    storedwor = x;
+                }
 
 
-                        }
-                    if(blackcounter>blackpixels) {
-                        blackpixels = blackcounter;
+            }
+            if (blackcounter > blackpixels) {
+                blackpixels = blackcounter;
 
-                        storedquadrant = (inputarray[0].length - distanceToFirstQuestionhorizontal) / numOptions*storedwor;
-                    }
-
-
-                    }
-                return storedquadrant;
+                storedquadrant = (inputarray[0].length - distanceToFirstQuestionhorizontal) / numOptions * storedwor;
             }
 
 
-    public ArrayList<Integer> readQuestions(String filepath , int thresholdbwtnquestions, int thresholdbwtwnsolutions, int distanceToFirstQuestionhorizontal, int numOptions,int distancetofirstquestionvertical){
-        DImage getquestions=RunTheFilter(filepath).get(0);
-        ArrayList<Integer> answerkey= new ArrayList<>();
-        short[][]inputarray=getquestions.getBWPixelGrid();
-        for(int i=distancetofirstquestionvertical;i<inputarray.length;i+=thresholdbwtnquestions) {
+        }
+        return storedquadrant;
+    }
+
+    public static DImage getimage(String filepath, int index) {
+        DImage getquestions = RunTheFilter(filepath).get(index);
+        return getquestions;
+
+    }
 
 
-            for (int j = distanceToFirstQuestionhorizontal; j < inputarray[i].length - (j + thresholdbwtwnsolutions); j++) {
-               int answer = blackcpixelsinregion(inputarray,thresholdbwtnquestions,thresholdbwtwnsolutions,i,j,numOptions,distanceToFirstQuestionhorizontal);
-               answerkey.add(answer);
+    public static ArrayList<Integer> readQuestions(DImage getquestions, int thresholdbwtnquestions, int thresholdbwtwnsolutions, int distanceToFirstQuestionhorizontal, int numOptions, int distancetofirstquestionvertical) {
+        ArrayList<Integer> answerkey = new ArrayList<>();
+        short[][] inputarray = getquestions.getBWPixelGrid();
+        for (int i = distancetofirstquestionvertical; i < inputarray.length; i += thresholdbwtnquestions) {
+
+
+            for (int j = distanceToFirstQuestionhorizontal; j < inputarray[i].length - (j + thresholdbwtwnsolutions); j += thresholdbwtnquestions) {
+                int answer = blackcpixelsinregion(inputarray, thresholdbwtnquestions, thresholdbwtwnsolutions, i, j, numOptions, distanceToFirstQuestionhorizontal);
+                answerkey.add(answer);
 
 
             }
@@ -81,17 +85,31 @@ public class FilterTest {
         return answerkey;
 
 
+    }
 
+    public static ArrayList<ArrayList<Integer>> readanswers(int numstudents, int thresholdbwtnquestions, int thresholdbwtwnsolutions, int distanceToFirstQuestionhorizontal, int numOptions, int distancetofirstquestionvertical) {
+        ArrayList<ArrayList<Integer>> studentanswer = new ArrayList<>();
+        for (int i = 1; i < numstudents; i++) {
+            DImage currentstudent = getimage("C:\\Users\\adame\\IdeaProjects\\ImageProcessingOpticalMark\\assets\\omrtest.pdf", i);
+            ArrayList<Integer> currentstu = readQuestions(currentstudent, thresholdbwtnquestions, thresholdbwtwnsolutions, distanceToFirstQuestionhorizontal, numOptions, distancetofirstquestionvertical);
+            studentanswer.add(currentstu);
 
-
-
-
-
-
-
-
-
+        }
+        return studentanswer;
 
     }
 
+
+    public static double percentcorrect(ArrayList<Integer> x, ArrayList<Integer> answerkey) {
+       int sum =0;
+       if(x.size()!=answerkey.size() )return 0;
+        for (int i = 0; i < x.size(); i++) {
+            if (x.get(i)==answerkey.get(i)){
+                sum++;
+            }
+
+        }
+        return (double)(sum)/ x.size();
+
+    }
 }
